cc730c0e850d63a98ebef558049f3a0ae5277aff
Peter Landoll <pwlandoll@gmail.com>
2016-02-16 01:03:39 -0500

    Mostly finished hw3. Some issues left with color box sizing on my phone.

--- Homework3/app/src/main/java/edu/jcu/plandoll16/homework3/MainActivity.java
+++ Homework3/app/src/main/java/edu/jcu/plandoll16/homework3/MainActivity.java
@@ -2,22 +2,27 @@ package edu.jcu.plandoll16.homework3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Integer heightMin, heightMax, widthMin, widthMax;
     private TextView heightMinText, heightMaxText, widthMinText, widthMaxText, colorBox;
     private RadioGroup buttonGroup;
+    private SeekBar widthBar, heightBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Set the min and max values for the color box
         heightMin = 50;
-        heightMax = 400;
         widthMin = 50;
-        widthMax = 400;
+        heightMax = 350;
+        widthMax = 350;
+        // Find the text views for the seek bars and set the text
         heightMinText = (TextView)findViewById(R.id.heightMinTextView);
         heightMaxText = (TextView)findViewById(R.id.heightMaxTextView);
         widthMinText = (TextView)findViewById(R.id.widthMinTextView);
@@ -26,6 +31,85 @@ public class MainActivity extends AppCompatActivity {
         heightMaxText.setText(heightMax.toString());
         widthMinText.setText(widthMin.toString());
         widthMaxText.setText(widthMax.toString());
+        // Set the color box and set its width and height
+        colorBox = (TextView)findViewById(R.id.colorBox);
+        setColorBoxWidth(widthMin);
+        setColorBoxHeight(heightMin);
+        // Pull selected color from radio group and set color box background
         buttonGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        buttonGroup.setOnCheckedChangeListener(checkedListener);
+        updateColors(buttonGroup.getCheckedRadioButtonId());
+        // Set the seek bars, connect the listeners
+        widthBar = (SeekBar)findViewById(R.id.widthSeekBar);
+        widthBar.setMax(widthMax);
+        widthBar.setProgress(widthMin);
+        widthBar.setOnSeekBarChangeListener(widthListener);
+        heightBar = (SeekBar)findViewById(R.id.heightSeekBar);
+        heightBar.setMax(heightMax);
+        heightBar.setProgress(heightMin);
+        heightBar.setOnSeekBarChangeListener(heightListener);
     }
+
+    private void updateColors(int checked) {
+        if (checked == R.id.redRadioButton) {
+            colorBox.setBackgroundColor(getResources().getColor(R.color.red));
+        } else if (checked == R.id.greenRadioButton) {
+            colorBox.setBackgroundColor(getResources().getColor(R.color.green));
+        } else if (checked == R.id.blueRadioButton) {
+            colorBox.setBackgroundColor(getResources().getColor(R.color.blue));
+        }
+    }
+
+    private int dpToPx(int dp) {
+        return (int) (dp * getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    private void setColorBoxWidth(int width) {
+        colorBox.setWidth(dpToPx(width));
+    }
+
+    private void setColorBoxHeight(int height) {
+        colorBox.setHeight(dpToPx(height));
+    }
+
+    private RadioGroup.OnCheckedChangeListener checkedListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            updateColors(checkedId);
+        }
+    };
+
+    private SeekBar.OnSeekBarChangeListener widthListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            setColorBoxWidth(widthMin + progress);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private SeekBar.OnSeekBarChangeListener heightListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            setColorBoxHeight(heightMin + progress);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
