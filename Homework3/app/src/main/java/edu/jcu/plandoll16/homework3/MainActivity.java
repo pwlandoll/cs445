44fa0c01b632fb99a8ff9c704568ea47a3ee48fa
Peter Landoll <pwlandoll@gmail.com>
2016-02-15 23:58:27 -0500

    Finished layout, started the constructor

--- Homework3/app/src/main/java/edu/jcu/plandoll16/homework3/MainActivity.java
+++ Homework3/app/src/main/java/edu/jcu/plandoll16/homework3/MainActivity.java
@@ -2,12 +2,30 @@ package edu.jcu.plandoll16.homework3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Integer heightMin, heightMax, widthMin, widthMax;
+    private TextView heightMinText, heightMaxText, widthMinText, widthMaxText, colorBox;
+    private RadioGroup buttonGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        heightMin = 50;
+        heightMax = 400;
+        widthMin = 50;
+        widthMax = 400;
+        heightMinText = (TextView)findViewById(R.id.heightMinTextView);
+        heightMaxText = (TextView)findViewById(R.id.heightMaxTextView);
+        widthMinText = (TextView)findViewById(R.id.widthMinTextView);
+        widthMaxText = (TextView)findViewById(R.id.widthMaxTextView);
+        heightMinText.setText(heightMin.toString());
+        heightMaxText.setText(heightMax.toString());
+        widthMinText.setText(widthMin.toString());
+        widthMaxText.setText(widthMax.toString());
+        buttonGroup = (RadioGroup)findViewById(R.id.radioGroup);
     }
 }
