55af7dd6a4fb6009e64910244944422a166c95e5
pwlandoll <pwlandoll@gmail.com>
2016-02-17 09:52:36 -0500

    Added container example

---
+++ ContainerExample/app/src/main/java/edu/jcu/plandoll16/containerexample/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.jcu.plandoll16.containerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup textSize;
+    private TextView message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textSize = (RadioGroup)findViewById(R.id.buttonGroup);
+        message = (TextView)findViewById(R.id.textView);
+
+        textSize.setOnCheckedChangeListener(sizeListener);
+    }
+
+    private RadioGroup.OnCheckedChangeListener sizeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            float size;
+            if (checkedId == R.id.smallButton) {
+                size = getResources().getDimension(R.dimen.smallText);
+            } else if (checkedId == R.id.mediumButton) {
+                size = getResources().getDimension(R.dimen.mediumText);
+            } else {
+                size = getResources().getDimension(R.dimen.largeText);
+            }
+            message.setTextSize(size);
+        }
+    };
+}
