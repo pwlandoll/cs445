e916b250da25d486ea067d1ff7fbcf07554df852
pwlandoll <pwlandoll@gmail.com>
2016-03-11 09:02:29 -0500

    Why did these not add before??

---
+++ LifeCycleExample/app/src/main/java/edu/jcu/plandoll16/lifecycleexample/MainActivity.java
@@ -0,0 +1,38 @@
+package edu.jcu.plandoll16.lifecycleexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> messages;
+    private ArrayAdapter<String> myAdapter;
+    private String createMessage, startMessage, resumeMessage, pauseMessage, stopMessage, restartMessage;
+    private ListView messageList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        myAdapter.add(createMessage);
+        myAdapter.notifyDataSetChanged();
+
+    }
+
+    private void initialize() {
+        messageList = (ListView)findViewById(R.id.messageListView);
+        messages = new ArrayList<String>();
+        myAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1);
+        messageList.setAdapter(myAdapter);
+        createMessage = getResources().getString(R.string.create);
+        startMessage = getResources().getString(R.string.start);
+        resumeMessage = getResources().getString(R.string.resume);
+        pauseMessage = getResources().getString(R.string.pause);
+        stopMessage = getResources().getString(R.string.stop);
+        restartMessage = getResources().getString(R.string.restart);
+    }
+}
