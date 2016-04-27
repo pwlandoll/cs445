42c8a1117123e60ad9e46dfe05a1c565063a2559
Peter Landoll <pwlandoll@gmail.com>
2016-04-26 22:05:34 -0400

    Added javadoc/comments, implemented fetching data from csv

--- PrinterLocator/app/src/main/java/edu/jcu/plandoll16/printerlocator/Printer.java
+++ PrinterLocator/app/src/main/java/edu/jcu/plandoll16/printerlocator/Printer.java
@@ -1,41 +1,50 @@
 package edu.jcu.plandoll16.printerlocator;
 
 /**
- * Created by Peter Landoll on 4/12/16.
+ * Printer class.
+ *
+ * Each printer has variables corresponding to the fields of the SQLite database.
+ *
+ * @author Peter Landoll <plandoll16@jcu.edu>
+ * @version 0.1
+ * @since 2016-4-12
  */
 public class Printer {
-    private String name, status;
     private double locationLatitude, locationLongitude;
+    private int statusCode;
+    private String name;
 
+    /**
+     * Constructor.
+     *
+     * Default name is "printer", location is the middle of the Admin building
+     */
     public Printer() {
         // Default name is "printer", location is middle of the Admin building
         this("printer", 41.490150, -81.531329);
     }
 
+    /**
+     * Constructor.
+     *
+     * Default location is the middle of the Admin building.
+     * @param name name of the printer
+     */
     public Printer(String name) {
         this(name, 41.490150, -81.531329);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param name name of the printer
+     * @param locationLatitude latitude of the location of the printer
+     * @param locationLongitude longitude of the location of the printer
+     */
     public Printer(String name, double locationLatitude, double locationLongitude) {
         this.name = name;
         this.locationLatitude = locationLatitude;
         this.locationLongitude = locationLongitude;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public double getLocationLatitude() {
@@ -52,5 +61,21 @@ public class Printer {
 
     public void setLocationLongitude(double locationLongitude) {
         this.locationLongitude = locationLongitude;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
